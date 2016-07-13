71aab1abcf46d71471917965acf032bb11c240cc
Kelly Anderson <kelly.j.anderson@gmail.com>
2016-07-13 18:28:10 -0500

    Update view to respond to menu actions.

--- Just a Menu/ViewController.swift
+++ Just a Menu/ViewController.swift
@@ -21,10 +21,27 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+
+    // Indicate that this view will have functions that
+    // respond to first responder actions.
+    override var acceptsFirstResponder: Bool{
+        return true
+    }
+
+    // "Save" is a pre-registered "First Responder" action
+    // Since acceptsFirstResponder was overriden and returns
+    // true, first responder actions will be handled by andy
+    // function with a matching name in this class.
+    func save(sender: NSMenuItem){
+        print("File >> Save clicked.");
+    }
     
-    func customAction(sender: NSMenuItem){
+    // Use @IBAction to indicate this function will
+    // attached to an action. Once this is marked with 
+    // @IBAction it will show up in the "Received Actions"
+    // list.
+    @IBAction func customAction(sender: NSMenuItem){
         print("Custom Menu Item clicked")
     }
 
 }
-
